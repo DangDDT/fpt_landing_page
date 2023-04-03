e41fcc40b83c33a18f7fb5900ddc9f61a867ca8e
Do Duong Tam Dang <dangddt@outlook.com>
2023-04-04 01:33:46 +0700

    [DangDDT] commit

--- src/components/register/register.data.tsx
+++ src/components/register/register.data.tsx
@@ -3,65 +3,380 @@ import { Program } from "@/interfaces/program";
 export const data: Array<Program> = [
   {
     id: 1,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Học kỳ nước ngoài tại Malaysia cùng Transition 6 dành cho tân sinh viên K18 Đại học FPT",
+    date: "13/10/2022",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-2-588x325.jpeg",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-0.jpeg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
   {
     id: 2,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Cơ hội học tập tại Pháp dành cho các kỹ sư phần mềm tương lai tại ĐH FPT",
+    date: "22/05/2021",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/05/185177312_10165021880520062_3780178681535126399_n-1-e1621652403919-588x325.jpeg",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/05/185177312_10165021880520062_3780178681535126399_n.jpeg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
   {
     id: 3,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Chương trình hè 9 tuần tại trường ĐH QG Singapore dành cho sinh viên năm cuối",
+    date: "25/02/2021",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/02/152012440_10164663092205062_8853326834642653114_o-1-e1614227611548-588x325.jpg",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/02/152012440_10164663092205062_8853326834642653114_o.jpg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
   {
     id: 4,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Học kì Japanese in Japan – Cơ hội nâng cao tiếng Nhật tại Đại học Rissho, Nhật Bản",
+    date: "07/04/2021",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/04/170337039_10164842232350062_205945936626501043_n-588x325.jpeg",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/04/170337039_10164842232350062_205945936626501043_n.jpeg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
   {
     id: 5,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Cơ hội học tập trao đổi tại Đài Loan dành cho sinh viên trường F",
+    date: "05/05/2020",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2020/05/95266809_1397734113745933_934366024961097728_o-588x325.jpg",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2020/05/95266809_1397734113745933_934366024961097728_o.jpg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
   {
     id: 6,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Thuỵ Điển – điểm đến của sinh viên trường F trong kỳ Exchange",
+    date: "31/03/2020",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2020/03/hk-nc-ngoai-thuy-si-588x325.jpg",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/media/2020/03/hk-nc-ngoai-thuy-si-325x325.jpg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
   {
     id: 7,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Học kỳ trao đổi tại Nhật Bản dành cho sinh viên Đại học FPT TP. HCM",
+    date: "10/03/2021",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/03/628-588x325.jpeg",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/03/157294144_10164719630960062_4563103666557693552_n.jpg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
   {
     id: 8,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Tour tham quan các trường Đại học tại Malaysia dành cho sinh viên K15",
+    date: "05/03/2020",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2020/03/88307182_2534412040107494_4496419228030599168_o-588x325.jpg",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2020/03/88307182_2534412040107494_4496419228030599168_o.jpg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
   {
     id: 9,
-    programName: "Tên chương trình",
-    date: "11/10/2000",
+    programName:
+      "Swinburne, Úc: điểm đến tiếp theo của sinh viên trường F trong chương trình “english level 6”",
+    date: "29/11/2019",
     imageUrl:
-      "https://hcmuni.fpt.edu.vn/Data/Sites/1/media/2020-kim-vi/seo/campus/1-truong-dai-hoc-fpt-tphcm/truong-dai-hoc-fpt-tp-hcm-(1).jpg",
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2019/11/4-588x325.png",
+    detailImageUrl:
+      "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-0.jpeg",
+    albumImageUrl: [
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-8.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-910.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-4.jpeg",
+        width: 910,
+        height: 763,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-7.jpeg",
+        width: 800,
+        height: 1000,
+      },
+      {
+        src: "https://daihoc.fpt.edu.vn/wp-content/uploads/2022/10/hoc_ky_tien_anh_o_nuoc_ngoai_sinh_vien_DhFPT-6.jpeg",
+        width: 800,
+        height: 1000,
+      },
+    ],
   },
 ];
